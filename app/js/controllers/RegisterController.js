d0bcf273385a356aaeddd9df223c719fd761cc83
Zhelyazko Stoyanov <zh.stoqnov@gmail.com>
2016-04-10 14:16:34 +0300

    login register fully implemented, view all ads aswell

--- app/js/controllers/RegisterController.js
+++ app/js/controllers/RegisterController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.controller('RegisterController',
-    function ($scope, townsService, authService, notifyService) {
+    function ($scope, $location, townsService, authService, notifyService) {
         $scope.userData = {townId: null};
         $scope.towns = townsService.getTowns();
 
@@ -9,7 +9,7 @@ app.controller('RegisterController',
             authService.register(userData,
                 function success() {
                     notifyService.showInfo("You have successfully registered");
-                    $location('/login');
+                    $location.path('/');
                 },
                 function error(err) {
                     notifyService.showError('User registration failed', err);
